139d0ee8bee6063e42dfdb1d73996c5bb808d616
JT Raber <JMANN240raber@gmail.com>
2021-05-20 11:33:25 -0400

    made a very basic mirror

---
+++ mirror.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, Response
+import cv2
+import imutils
+import sys
+from imutils.video import VideoStream
+import time
+import threading
+import os
+import numpy as np
+
+onpi = os.uname()[1] == 'raspberrypi'
+
+if onpi:
+    from picamera.array import PiRGBArray
+    from picamera import PiCamera
+
+if onpi:
+    camera = PiCamera()
+    resolution = (480, 368)
+    framerate = 30
+    camera.resolution = resolution
+    camera.framerate = framerate
+    rawCapture = PiRGBArray(camera, size=resolution)
+    frame = np.zeros((resolution[1], resolution[0], 3), np.uint8)
+else:
+    camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    frame = np.zeros((int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(camera.get(cv2.CAP_PROP_FRAME_WIDTH)), 3), np.uint8)
+
+running = True
+tracking = True
+
+def getFrame():
+    global camera, frame, running, onpi
+    if onpi:
+        global rawCapture
+        for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+            frame = f.array
+            rawCapture.truncate(0)
+            if not running:
+                break
+    else:
+        while running:
+            success, frame = camera.read()
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+@app.route('/')
+def index():
+    return render_template('mirror.html')
+
+@app.route('/api/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    global running
+    running = False
+    func = request.environ.get('werkzeug.server.shutdown')
+    func()
+    return "200"
+
+def gen_frames():
+    global frame, running
+    while running:
+        ret, prebuf = cv2.imencode('.jpg', frame)
+        buffer = prebuf.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + buffer + b'\r\n')  # concat frame one by one and show result
+        time.sleep(1/30)
+
+getFrameThread = threading.Thread(target=getFrame)
+getFrameThread.name = "get frame"
+
+serverThread = threading.Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 8050})
+serverThread.name = "server"
+
+if __name__ == "__main__":
+    getFrameThread.start()
+    serverThread.start()
+    getFrameThread.join()
+    serverThread.join()
